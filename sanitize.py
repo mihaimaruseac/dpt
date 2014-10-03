036e136eb41ac1ede06b890472e560283013099e
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-10-03 00:01:03 -0400

    Argument passing, first attempt.

--- sanitize.py
+++ sanitize.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import sys
+
 import numpy
 import scipy
 
+class Args:
+    def __init__(self, epsilon):
+        self.epsilon = epsilon
+
+def print_call(args):
+    print "Called with\n\tepsilon = {:5.2f}".format(args.epsilon)
+
+def usage():
+    print sys.argv[0], ": Differentially private trajectory mining"
+    print "Usage:"
+    print "\t{0} epsilon".format(sys.argv[0])
+    sys.exit(-1)
+
 def main():
-    print "Nothing yet"
+    if len(sys.argv) != 2:
+        usage()
+    args = Args(float(sys.argv[1]))
+    print_call(args)
 
 if __name__ == '__main__':
     main()
