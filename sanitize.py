c724be769ef35d1694d9c4f50773e36e5bb9bead
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-10-05 21:14:00 -0400

    More handwritten tests

--- sanitize.py
+++ sanitize.py
@@ -25,11 +25,42 @@ def main():
     args = Args(float(sys.argv[1]))
     print_call(args)
     # TODO:  build graph
-    graph = numpy.ones(3) - numpy.eye(3)
+    nodes = 3
+    lmax = 3
+    graph = numpy.ones(nodes) - numpy.eye(nodes)
     print "Graph is:\n", graph
-    transactions = [(1, 2), (1, 2, 3), (2, 3), (1, 3)]
+    transactions = [[1, 2], [1, 2, 3], [2, 3], [1, 3]]
     print "Transactions:\n", transactions
+    real_pairs = {}
+    for i in xrange(1, nodes+1):
+        real_pairs[(0, i)] = real_pairs[(i, 0)] = 0
+        for j in xrange(1, nodes+1):
+            if graph[i-1][j-1]:
+                real_pairs[(i, j)] = 0
+    for t in transactions:
+        t = [0] + t + [0] # add extra edge
+        for edge in zip(t, t[1:]):
+            if not real_pairs.has_key(edge):
+                raise Exception("Invalid transaction found " + t)
+            real_pairs[edge] += 1
+    print "Real pair counts:\n", real_pairs
+    real_pairs_array = numpy.zeros([nodes + 1, nodes + 1])
+    for k in real_pairs:
+        real_pairs_array[k] = real_pairs[k]
+    print real_pairs_array
     # add noise to pairs
+    noisy_pairs = {}
+    for k in real_pairs:
+        noisy_pairs[k] = real_pairs[k] + numpy.random.laplace(scale=(lmax+1)/args.epsilon)
+    noisy_pairs = {(0, 1): 3.03, (0, 2): 1.03, (0, 3): 0.27,
+            (1, 0): -0.35, (1, 2): 0.93, (1, 3): -0.53,
+            (2, 0): 1.03, (2, 1): 0, (2, 3): 2.01,
+            (3, 0): 2.72, (3, 1): 0.18, (3, 2):-0.25}
+    print "Noisy pair counts:\n", noisy_pairs
+    noisy_pairs_array = numpy.zeros([nodes + 1, nodes + 1])
+    for k in noisy_pairs:
+        noisy_pairs_array[k] = noisy_pairs[k]
+    print noisy_pairs_array
     # compute approximation
     # get errors
 
